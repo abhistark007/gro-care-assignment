80ef555a4689f9b7bd6ae80f3d7b8b3f112cf65d
abhistark007 <abhi.kumar2019@vitstudent.ac.in>
2023-05-22 11:43:41 +0530

    Converted LeftSideBar to Component

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import WhatshotSharpIcon from '@mui/icons-material/WhatshotSharp';
-import HomeSharpIcon from '@mui/icons-material/HomeSharp';
+import LeftSideBar from '../components/LeftSideBar'
+
 
 
 
@@ -8,26 +8,7 @@ function HomePage() {
   return (
     <div className='flex min-h-screen bg-[#15141B]'>
         {/* Left Side */}
-        <div className='flex-[0.2] flex flex-col bg-[#191922]'>
-            <ul className='flex flex-col text-white mt-5'>
-                <li className='flex gap-2 items-center px-10 py-2 duration-200 hover:bg-purple-500 cursor-pointer'>
-                    <p>
-                        <HomeSharpIcon/>
-                    </p>
-                    <p>
-                        Home
-                    </p>
-                </li>
-                <li className='flex gap-2 items-center px-10 py-2 duration-200 hover:bg-purple-500 cursor-pointer'>
-                    <p>
-                        <WhatshotSharpIcon/>
-                    </p>
-                    <p>
-                        Trending
-                    </p>
-                </li>
-            </ul>
-        </div>
+        <LeftSideBar/>
 
 
 
