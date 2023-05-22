28611fd693d4060f27cc343bce088846c5ae8266
abhistark007 <abhi.kumar2019@vitstudent.ac.in>
2023-05-22 13:11:02 +0530

    Create UserInfoBottomPart.js

---
+++ src/components/UserInfoBottomPart.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function UserInfoBottomPart({imageList}) {
+  return (
+    <div className='userInfoBottomClass flex overflow-x-scroll rounded-xl px-4 py-2 gap-3 '>
+        {
+            imageList.map((image)=>{
+                return <img src={image} alt='demo' className='rounded-xl w-[50%]'/>
+            })
+        }
+    </div>
+  )
+}
+
+export default UserInfoBottomPart
