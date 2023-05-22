c65c009726523c5da4088974ac7ba3bbf2320edd
abhistark007 <abhi.kumar2019@vitstudent.ac.in>
2023-05-22 13:10:11 +0530

    Added px-5

--- src/components/RightSideBar.js
+++ src/components/RightSideBar.js
@@ -3,7 +3,7 @@ import AdsBannerAndUserInfo from './AdsBannerAndUserInfo'
 
 function RightSideBar() {
   return (
-    <div className='flex-[0.8] flex flex-col'>
+    <div className='flex-[0.8] flex flex-col px-5'>
         <AdsBannerAndUserInfo/>
     </div>
   )
