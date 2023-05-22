4218dbb4f7a5e9697fd93cd96bc2ba3990e4e523
abhistark007 <abhi.kumar2019@vitstudent.ac.in>
2023-05-22 17:08:10 +0530

    Revert "Create RightSideBar.css"
    This reverts commit c30048b2ab03da679a6a6894c6c51e60f8b33954.

--- src/components/RightSideBar.js
+++ src/components/RightSideBar.js
@@ -1,12 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 import AdsBannerAndUserInfo from './AdsBannerAndUserInfo'
 import VideoDisplaySection from './VideoDisplaySection'
+import './RightSideBar.css'
 
 function RightSideBar() {
+  const [loading,setLoading]=useState(false);
+  const [page,setPage]=useState(0);
+  const [posts,setPosts]=useState([]);
+
+  const getPosts=()=>{
+    setLoading(true);
+    try{
+      fetch(`https://internship-service.onrender.com/videos?page=${page}`)
+      .then((response)=>response.json())
+      .then((result)=> {
+        if(result.message==="Success"){
+          setPosts(result.data.posts);
+          console.log(result.data.posts);
+        }else{
+          setPosts([]);
+        }
+      })
+    }catch(e){
+      console.log("Error occured while using getPosts");
+      console.log(e.message);
+    }
+    setLoading(false);
+  }
+
+
   return (
     <div className='flex-[0.8] flex flex-col px-5 gap-5'>
         <AdsBannerAndUserInfo/>
-        <VideoDisplaySection />
+        {
+          (loading===true)?
+          (<div className='flex justify-center items-center'>
+            <div class="custom-loader"></div>
+          </div>)
+          :(posts.length===0?(<div className='flex justify-center items-center text-white text-3xl'>No Posts Found :|</div>)
+          :(<VideoDisplaySection posts={posts}/>))
+        }
     </div>
   )
 }
