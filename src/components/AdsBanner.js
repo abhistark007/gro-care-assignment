52bf09725e421693212ec642b3efca210dcea089
abhistark007 <abhi.kumar2019@vitstudent.ac.in>
2023-05-22 12:05:32 +0530

    Created AdsBanner Component

---
+++ src/components/AdsBanner.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AdsBanner({image}) {
+  return (
+    <div className='flex-[0.7] flex rounded-[2rem] shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px] duration-200 hover:scale-110 cursor-pointer'>
+        <img src={image} alt='ad banner' className='rounded-[2rem] h-72 w-full object-cover object-top'/>
+    </div>
+  )
+}
+
+export default AdsBanner
