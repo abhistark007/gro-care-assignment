946ee57581c16940da4eb797c64f6f78598e251d
abhistark007 <abhi.kumar2019@vitstudent.ac.in>
2023-05-22 11:48:23 +0530

    Added AdsBanner & UserInfo Section

--- src/components/RightSideBar.js
+++ src/components/RightSideBar.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import AdsBannerAndUserInfo from './AdsBannerAndUserInfo'
 
 function RightSideBar() {
   return (
     <div className='flex-[0.8] flex flex-col'>
-        RightSideBar
+        <AdsBannerAndUserInfo/>
     </div>
   )
 }
