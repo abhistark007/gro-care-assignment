ea7ba50815ad66e1b16febe1674d6c0f3800b04c
abhistark007 <abhi.kumar2019@vitstudent.ac.in>
2023-05-22 17:56:25 +0530

    Update UserInfoBottomPart.js
    Added index key during mapping

--- src/components/UserInfoBottomPart.js
+++ src/components/UserInfoBottomPart.js
@@ -4,8 +4,8 @@ function UserInfoBottomPart({imageList}) {
   return (
     <div className='userInfoBottomClass flex overflow-x-scroll rounded-xl px-4 py-2 gap-3 '>
         {
-            imageList.map((image)=>{
-                return <img src={image} alt='demo' className='rounded-xl w-[50%]'/>
+            imageList.map((image,index)=>{
+                return <img key={index} src={image} alt='demo' className='rounded-xl w-[50%]'/>
             })
         }
     </div>
