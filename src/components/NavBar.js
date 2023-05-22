92320b623c6b142f81c75e63cd80c92d94634733
abhistark007 <abhi.kumar2019@vitstudent.ac.in>
2023-05-22 09:12:24 +0530

    Created NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import './NavBar.css'
+import appLogo from '../assets/app_logo.svg'
+import { Avatar } from '@mui/material'
 
 function NavBar() {
-  return (
-    <div className='wrapper h-12 w-full'>NavBar</div>
-  )
+    return (
+        <div className='wrapper h-16 text-white flex justify-between items-center px-10'>
+            <img src={appLogo} alt='gro.care' />
+
+            <Avatar className='!rounded-xl' sx={{ bgcolor: "purple" }} variant="square" >
+                AK
+            </Avatar>
+
+        </div>
+    )
 }
 
 export default NavBar
