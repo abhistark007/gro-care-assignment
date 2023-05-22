0b56ddea98fd5266151fd821132a61f8a515200b
abhistark007 <abhi.kumar2019@vitstudent.ac.in>
2023-05-22 12:26:05 +0530

    Added UserInfo Component

--- src/components/AdsBannerAndUserInfo.js
+++ src/components/AdsBannerAndUserInfo.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import AdsBanner from './AdsBanner'
 import adsBannerImage from '../assets/adBannerImage.png'
+import UserInfo from './UserInfo'
 
 function AdsBannerAndUserInfo() {
   return (
     <div className='flex'>
         <AdsBanner image={adsBannerImage}/>
+        <UserInfo/>
     </div>
   )
 }
