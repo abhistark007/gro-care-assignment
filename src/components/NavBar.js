b4c4a30388f7e50f5860baf72f03397685d980f5
abhistark007 <abhi.kumar2019@vitstudent.ac.in>
2023-05-22 13:08:53 +0530

    Added purple border to input field

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,7 +8,7 @@ function NavBar() {
     return (
         <div className='bg-[#15141B] py-5 text-white flex justify-between items-center px-10'>
             <img src={appLogo} alt='gro.care' />
-            <div className='flex border-2 focus:border-blue-600 rounded-3xl w-[50%]'>
+            <div className='flex border-2 border-purple-500 rounded-3xl w-[50%]'>
             <input placeholder='Search' className='rounded-l-3xl py-2 px-3 outline-none bg-[#1F1E24] w-full'/>
             <div className='flex justify-between items-center rounded-r-3xl py-2 px-2 bg-purple-500 cursor-pointer duration-150 hover:scale-90'>
                 <SearchSharpIcon/>
