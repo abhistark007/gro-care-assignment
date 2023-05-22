2a66c6f0d50255205f795291b967670f4c317c0e
abhistark007 <abhi.kumar2019@vitstudent.ac.in>
2023-05-22 17:57:09 +0530

    Update VideoDisplaySection.js

--- src/components/VideoDisplaySection.js
+++ src/components/VideoDisplaySection.js
@@ -1,46 +1,26 @@
 import React from 'react'
 import VideoCard from './VideoCard'
 
-function VideoDisplaySection() {
+function VideoDisplaySection({posts}) {
   
 
 
 
 
   return (
-    <div className='flex flex-wrap gap-10  '>
-        <VideoCard 
-        thumbnail={"https://cdn.gro.care/53d5e59f202d_1683463530883.jpeg"}
-        title={"Random Title assd  sda adsa dsad asdsa d asd i am god and i wont stop"}
-        creatorName={"soniyaassssssssssssssssssssssssssssssssssssss"}
-        creatorPic={"https://cdn.gro.care/6ce188269916_1667990600681.bin"}
-        reactionCount={31}
-        liked={true}
-        />
-        <VideoCard 
-        thumbnail={"https://cdn.gro.care/17af050fdd9f_1683462551951.jpeg"}
-        title={"Random Title"}
-        creatorName={"soniya"}
-        creatorPic={"https://cdn.gro.care/6ce188269916_1667990600681.bin"}
-        reactionCount={31}
-        liked={false}
-        />
-        <VideoCard 
-        thumbnail={"https://cdn.gro.care/53d5e59f202d_1683463530883.jpeg"}
-        title={"Random Title"}
-        creatorName={"soniya"}
-        creatorPic={"https://cdn.gro.care/6ce188269916_1667990600681.bin"}
-        reactionCount={31}
-        liked={true}
-        />
-        <VideoCard 
-        thumbnail={"https://cdn.gro.care/53d5e59f202d_1683463530883.jpeg"}
-        title={"Random Title"}
-        creatorName={"soniya"}
-        creatorPic={"https://cdn.gro.care/6ce188269916_1667990600681.bin"}
-        reactionCount={31}
-        liked={true}
-        />
+    <div className='flex flex-wrap gap-10  justify-center'>
+        {
+          posts.map((post,index)=>{
+            return <VideoCard key={index}
+            thumbnail={post.submission.thumbnail}
+            title={post.submission.title??"NA"}
+            creatorName={post.creator.name??"NA"}
+            creatorPic={post.creator.pic}
+            reactionCount={post.reaction.count??0}
+            liked={post.reaction.voted??false}
+            />
+          })
+        }
     </div>
   )
 }
