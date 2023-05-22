8575fda71ec267f05e0026cee914aa52207c2bed
abhistark007 <abhi.kumar2019@vitstudent.ac.in>
2023-05-22 16:51:03 +0530

    Added VideoDisplaySection

--- src/components/RightSideBar.js
+++ src/components/RightSideBar.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import AdsBannerAndUserInfo from './AdsBannerAndUserInfo'
+import VideoDisplaySection from './VideoDisplaySection'
 
 function RightSideBar() {
   return (
-    <div className='flex-[0.8] flex flex-col px-5'>
+    <div className='flex-[0.8] flex flex-col px-5 gap-5'>
         <AdsBannerAndUserInfo/>
+        <VideoDisplaySection />
     </div>
   )
 }
