c8c6f75f7f66d9f6ed7bb0e5161f4777596256e8
abhistark007 <abhi.kumar2019@vitstudent.ac.in>
2023-05-22 21:50:42 +0530

    Added goto home link onClicking logo

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,11 +3,12 @@ import './NavBar.css'
 import appLogo from '../assets/app_logo.svg'
 import { Avatar } from '@mui/material'
 import SearchSharpIcon from '@mui/icons-material/SearchSharp';
+import { Link } from 'react-router-dom';
 
 function NavBar() {
     return (
         <div className='bg-[#15141B] py-5 text-white flex justify-between items-center px-10'>
-            <img src={appLogo} alt='gro.care' />
+            <Link to={"/"}><img src={appLogo} alt='gro.care' /></Link>
             <div className='flex border-2 border-purple-500 rounded-3xl w-[50%]'>
             <input placeholder='Search' className='rounded-l-3xl py-2 px-3 outline-none bg-[#1F1E24] w-full'/>
             <div className='flex justify-between items-center rounded-r-3xl py-2 px-2 bg-purple-500 cursor-pointer duration-150 hover:scale-90'>
