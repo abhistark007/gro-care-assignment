d65cf0e5b9ab743a514e02449f92391025c73089
abhistark007 <abhi.kumar2019@vitstudent.ac.in>
2023-05-22 19:30:07 +0530

    Update RightSideBar.js
    Added nextPage & prevPage Logic and also made useEffect change on pageChange

--- src/components/RightSideBar.js
+++ src/components/RightSideBar.js
@@ -2,11 +2,23 @@ import React, { useEffect, useState } from 'react'
 import AdsBannerAndUserInfo from './AdsBannerAndUserInfo'
 import VideoDisplaySection from './VideoDisplaySection'
 import './RightSideBar.css'
+import { toast } from 'react-hot-toast';
 
 function RightSideBar() {
   const [loading,setLoading]=useState(false);
   const [page,setPage]=useState(0);
   const [posts,setPosts]=useState([]);
+
+  const nextPage=()=>{
+      setPage(page=>page+1);
+  }
+  const prevPage=()=>{
+    if(page===0){
+      toast.success("First Page Reached");
+      return;
+    }
+    setPage(page=>page-1);
+  }
 
   const getPosts=()=>{
     setLoading(true);
@@ -15,6 +27,10 @@ function RightSideBar() {
       .then((response)=>response.json())
       .then((result)=> {
         if(result.message==="Success"){
+          if(result.data.posts.length===0){
+            toast.success("Final Page Reached!");
+            setPage(0);
+          }
           setPosts(result.data.posts); 
           console.log(result.data.posts);
         }else{
@@ -30,9 +46,7 @@ function RightSideBar() {
 
   useEffect(()=>{
     getPosts();
-
-    
-  },[])
+  },[page])
 
 
   return (
@@ -46,6 +60,12 @@ function RightSideBar() {
           :(posts.length===0?(<div className='flex justify-center items-center text-white text-3xl'>No Posts Found :|</div>)
           :(<VideoDisplaySection posts={posts}/>))
         }
+
+        <div className='flex justify-center gap-10 items-center mt-5'>
+              <button onClick={prevPage} className='bg-purple-500 text-white px-4 py-2 rounded-xl duration-200 hover:scale-110'>Prev</button>
+              <p className='text-white text-xl'>{page+1}</p>
+              <button onClick={nextPage} className='bg-purple-500 text-white px-4 py-2  rounded-xl  duration-200 hover:scale-110'>Next</button>
+        </div>
     </div>
   )
 }
