c335126f8108eba3911b927aaafbc51aee9c8b8b
abhistark007 <abhi.kumar2019@vitstudent.ac.in>
2023-05-22 16:50:49 +0530

    Changed Image position to topleft

--- src/components/AdsBanner.js
+++ src/components/AdsBanner.js
@@ -3,7 +3,7 @@ import React from 'react'
 function AdsBanner({image}) {
   return (
     <div className='flex-[0.65] flex rounded-[2rem] shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px] duration-200 hover:z-20 hover:scale-110 cursor-pointer'>
-        <img src={image} alt='ad banner' className='rounded-[2rem] h-72 w-full object-cover object-top'/>
+        <img src={image} alt='ad banner' className='rounded-[2rem] h-72 w-full object-cover object-left-top'/>
     </div>
   )
 }
