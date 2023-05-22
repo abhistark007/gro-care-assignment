60de6bb27aace77ebd0966e204f15bee5d078bdc
abhistark007 <abhi.kumar2019@vitstudent.ac.in>
2023-05-22 08:50:58 +0530

    Added react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import HomePage from './pages/HomePage';
+import VideoPlayerPage from './pages/VideoPlayerPage';
 
 function App() {
   return (
     <div className="App">
       
+      <Routes>
+        <Route path='/' element={<HomePage/>}/>
+        <Route path='/videoplayer/:id'element={<VideoPlayerPage/>}/>
+      </Routes>
     </div>
   );
 }
