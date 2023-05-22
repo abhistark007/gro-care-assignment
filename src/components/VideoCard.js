25d82db5985da5d6eb5086c9398403ce5356e23e
abhistark007 <abhi.kumar2019@vitstudent.ac.in>
2023-05-22 17:57:02 +0530

    Update VideoCard.js
    Converted title and creatorName to String as .length property was not working on them

--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -4,15 +4,16 @@ import FavoriteBorderSharpIcon from '@mui/icons-material/FavoriteBorderSharp';
 import FavoriteSharpIcon from '@mui/icons-material/FavoriteSharp';
 
 function VideoCard({thumbnail,title,creatorName,reactionCount,creatorPic,liked}) {
+  
   return (
     <div className='flex flex-col gap-4 w-[15rem] rounded-[2rem] border-2 bg-[#15141B] border-purple-500 shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px] duration-200 hover:z-20 hover:scale-110 cursor-pointer'>
         <img src={thumbnail} alt={title} className='  rounded-t-[2rem] w-[15rem] '/>
         <div className='flex gap-2 px-2 py-2'>
             <Avatar src={creatorPic}/>
             <div className='flex flex-col '>
-                <p className='text-white'>{title.length>20?title.substring(0,20)+"...":title}</p>
+                <p className='text-white'>{String(title).length>20?(String(title).substring(0,20)+"..."):title}</p>
                 <div className='flex justify-between text-[#7F7D8D] flex-col '>
-                    <p className=''>{creatorName.length>20?creatorName.substring(0,20)+"...":creatorName}</p>
+                    <p className=''>{String(creatorName).length>20?(String(creatorName).substring(0,20)+"..."):creatorName}</p>
                     <div className='flex gap-3'>
                       <p className=''>{reactionCount} reactions</p>
                       {
