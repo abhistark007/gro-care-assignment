e7ef217555d6228ac4d6a9aaaa8069e9c6c4e8fb
abhistark007 <abhi.kumar2019@vitstudent.ac.in>
2023-05-22 21:51:28 +0530

    Added Framer Motion animation

--- src/pages/VideoPlayerPage.js
+++ src/pages/VideoPlayerPage.js
@@ -1,12 +1,24 @@
 import React from 'react'
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
+import ReactPlayer from 'react-player'
+import { motion } from 'framer-motion'
 
 function VideoPlayerPage() {
-  const {mediaUrl}=useParams();
+  const { mediaurl } = useParams();
+  const navigate = useNavigate();
+
+  const videoUrl = `https://cdn.gro.care/${mediaurl}`
+  console.log(videoUrl);
   return (
-    <div>
-        
-    </div>
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      className='h-screen bg-[#15141B]'>
+      <button onClick={() => navigate(-1)}>Go back</button>
+      <ReactPlayer url={videoUrl} loop={true} playing={true} />
+
+    </motion.div>
   )
 }
 
