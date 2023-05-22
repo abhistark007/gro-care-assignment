57678061499b1b09f888de7f69a309125906624d
abhistark007 <abhi.kumar2019@vitstudent.ac.in>
2023-05-22 21:50:25 +0530

    Configured Framer Motion for page transition Animation

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useLocation } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/HomePage';
 import VideoPlayerPage from './pages/VideoPlayerPage';
 import NavBar from './components/NavBar';
-import {Toaster} from 'react-hot-toast';
+import { Toaster } from 'react-hot-toast';
+import { AnimatePresence } from 'framer-motion';
 
 function App() {
+  const location = useLocation();
   return (
     <div className="App flex flex-col">
-      <NavBar/>
-      <Routes>
-        <Route path='/' element={<HomePage/>}/>
-        <Route path='/videoplayer/:mediaUrl'element={<VideoPlayerPage/>}/>
-      </Routes>
-      <Toaster/>
+      <NavBar />
+      <AnimatePresence>
+        <Routes location={location} key={location.pathname}>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/videoplayer/:mediaurl' element={<VideoPlayerPage />} />
+        </Routes>
+      </AnimatePresence>
+      <Toaster />
     </div>
   );
 }
