b4c5de893d4f858fd39c812c4fbbb1ba854880ae
abhistark007 <abhi.kumar2019@vitstudent.ac.in>
2023-05-22 09:11:55 +0530

    Added NavBar in App.js

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/HomePage';
 import VideoPlayerPage from './pages/VideoPlayerPage';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="App flex flex-col">
+      <NavBar/>
       <Routes>
         <Route path='/' element={<HomePage/>}/>
         <Route path='/videoplayer/:id'element={<VideoPlayerPage/>}/>
